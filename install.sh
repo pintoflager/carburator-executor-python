05370a75e991e6507f393bef3a125aebed994a15
pintoflager <jaakko0606@gmail.com>
2022-12-11 18:41:17 -0500

    init scripts for integrations, project install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Python needs python
+if ! carburator fn integration-installed python3; then
+  carburator print terminal error "Missing required program python. Please install it" \
+    "before proceeding." && exit 120
+fi
