3e1f2f123729da231d7162828eee2bbaad5d5bc4
pintoflager <jaakko0606@gmail.com>
2023-01-22 19:58:03 -0500

    take executors into play

--- install.sh
+++ install.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
 # Python needs python
-err="Missing required program python. Please install it before proceeding."
-
-# If remotes don't have carburator installed we can't use it's methods.
-if ! which caburator &> /dev/null; then
-  if ! which python3 &> /dev/null; then
-    echo "$err" && exit 120
-  fi
-  
-  exit 100
-fi
-
 if ! carburator fn integration-installed python3; then
-  carburator print terminal error "$err" && exit 120
+  carburator print terminal error \
+    "Missing required program python. Please install it before proceeding."
+    exit 110
 fi
