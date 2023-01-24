a06c042af4f82692ae8c02a794b7afd1b6443235
pintoflager <jaakko0606@gmail.com>
2023-01-24 10:04:13 -0500

    package dependencies, registration dep checks

--- executor/sh/install.sh
+++ executor/sh/install.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Python needs python
 if ! carburator fn integration-installed python3; then
   carburator print terminal error \
     "Missing required program python. Please install it before proceeding."
-    exit 110
+    exit 120
 fi
