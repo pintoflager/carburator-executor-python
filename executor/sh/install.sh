23a920c76799e5693f2bb9539295e38ba4047459
pintoflager <jaakko0606@gmail.com>
2023-02-10 17:53:00 -0500

    Modified package dependency handling. And everything else

--- executor/sh/install.sh
+++ executor/sh/install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+platform="$1"
+
+# App installation tasks on commander node. Runs first
+if [ "$platform" = 'commander' ]; then
+    carburator print terminal info "Executing install script on $platform"
+fi
+
+# App installation tasks on remote worker node.
+if [ "$platform" = 'worker' ]; then
+    carburator print terminal info "Executing install script on $platform"
+fi
+
 # Python needs python
 if ! carburator fn integration-installed python3; then
   carburator print terminal error \
