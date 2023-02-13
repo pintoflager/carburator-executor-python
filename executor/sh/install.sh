7e5227cfda5408ad16b34b0233727ffe986d10f7
pintoflager <jaakko0606@gmail.com>
2023-02-13 18:37:56 -0500

    Oh yeah and that thing

--- executor/sh/install.sh
+++ executor/sh/install.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
 
-platform="$1"
+role="$1"
 
-# App installation tasks on commander node. Runs first
-if [ "$platform" = 'commander' ]; then
-    carburator print terminal info "Executing install script on $platform"
+# App installation tasks on a local client node. Runs first
+if [ "$role" = 'client' ]; then
+    carburator print terminal info "Executing install script on $role"
 fi
 
 # App installation tasks on remote worker node.
-if [ "$platform" = 'worker' ]; then
-    carburator print terminal info "Executing install script on $platform"
+if [ "$role" = 'worker' ]; then
+    carburator print terminal info "Executing install script on $role"
+fi
+
+# App installation tasks on remote commander node.
+if [ "$role" = 'commander' ]; then
+    carburator print terminal info "Executing install script on $role"
 fi
 
 # Python needs python
-if ! carburator fn integration-installed python3; then
+if ! carburator has program python3; then
   carburator print terminal error \
     "Missing required program python. Please install it before proceeding."
     exit 120
